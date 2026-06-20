903c1c64fb6c9b948545e94b4afe13f892700a31
khushbooSoni-123 <khushboosoni040@gmail.com>
2026-06-20 21:09:58 +0530

    learn functions in js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("khushboo");
+function Swap() {
+var age = 10 ;
+console.log(age);
+
+}
+Swap()
+
+//block scope
+{
+    var age = "hgfgf"  
+    
+}
+ console.log(age);
+
+
+let a = function () {
+var age = 10 ;
+console.log(age);
+}
+
+//fat arrow func
+
+let b= ()=>{
+
+}
+
+
+
